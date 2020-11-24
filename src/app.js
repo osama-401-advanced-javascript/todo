8c9a6f3ef7b2586af62cb38a4719c950555c73eb
Othabteh <o.thabteh@gmail.com>
2020-11-24 22:39:44 +0200

    lab33

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import SettingsProvider from './context/settings';
 
 import ToDo from './components/todo/todo.js';
 
 export default function App() {
   return (
     <>
-      <ToDo />
+      <SettingsProvider>
+        <ToDo />
+      </SettingsProvider>
     </>
   );
 }
